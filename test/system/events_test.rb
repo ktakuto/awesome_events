0706c06b802d2d25f0bae066e81c294207fc95cc
takuto <sg.epk.1231@gmail.com>
2021-01-08 16:36:13 +0900

    fix events_test.rb

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -7,14 +7,7 @@ class EventsTest < ApplicationSystemTestCase
     assert_selector "h1", text: event.name
   end
 
-  test "/event/new ページを表示" do
-    sign_in_as(FactoryBot.create(:user))
-
-    visit new_event_url
-    assert_selector "h1", text: "イベント作成"
-  end
-
-  test "/event/new ページでフォームへ記入して登録" do
+  test "/events/new ページでフォームへ記入して登録" do
     sign_in_as(FactoryBot.create(:user))
 
     visit new_event_url
@@ -28,18 +21,18 @@ class EventsTest < ApplicationSystemTestCase
     end_at = start_at + 3.hour
 
     start_at_field = "event_start_at"
-    select start_at.strftime("%Y"), from: "#{start_at_field}_1i" # 年
-    select I18n.l(start_at, format: '%B'), from: "#{start_at_field}_2i" # 月
+    select start_at.strftime("%Y"),  from: "#{start_at_field}_1i" # 年
+    select I18n.l(start_at, format: '%B'),  from: "#{start_at_field}_2i" # 月
     select start_at.strftime("%-d"), from: "#{start_at_field}_3i" # 日
-    select start_at.strftime("%H"), from: "#{start_at_field}_4i" # 時
-    select start_at.strftime("%M"), from: "#{start_at_field}_5i" # 分
+    select start_at.strftime("%H"),  from: "#{start_at_field}_4i" # 時
+    select start_at.strftime("%M"),  from: "#{start_at_field}_5i" # 分
 
     end_at_field = "event_end_at"
-    select end_at.strftime("%Y"), from: "#{end_at_field}_1i" # 年
-    select I18n.l(end_at, format: '%B'), from: "#{end_at_field}_2i" # 月
+    select end_at.strftime("%Y"),  from: "#{end_at_field}_1i" # 年
+    select I18n.l(end_at, format: '%B'),  from: "#{end_at_field}_2i" # 月
     select end_at.strftime("%-d"), from: "#{end_at_field}_3i" # 日
-    select end_at.strftime("%H"), from: "#{end_at_field}_4i" # 時
-    select end_at.strftime("%M"), from: "#{end_at_field}_5i" # 分
+    select end_at.strftime("%H"),  from: "#{end_at_field}_4i" # 時
+    select end_at.strftime("%M"),  from: "#{end_at_field}_5i" # 分
 
     click_on "登録する"
     assert_selector "div.alert", text: "作成しました"
@@ -54,18 +47,6 @@ class EventsTest < ApplicationSystemTestCase
         click_on "イベントを削除する"
       end
       assert_selector "div.alert", text: "削除しました"
-    end
-  end
-
-  test "/ ページを表示して、未来のイベントは表示、過去のイベントは非表示であること" do
-    future_event = FactoryBot.create(:event, start_at: Time.zone.now + 3.day)
-    # past_event = FactoryBot.create(:event, start_at: Time.zone.now - 1.day)
-    past_event = FactoryBot.create(:event, start_at: Time.zone.now + 1.day)
-
-    travel_to Time.zone.now + 2.day do
-      visit root_url
-      assert_selector "h5", text: future_event.name
-      assert_no_selector "h5", text: past_event.name
     end
   end
 end
